419d73be7d2a179265690e28897852fd6f1a3bdc
DarKTeaMoNe <alimomeni200900@gmail.com>
2016-10-07 21:57:50 +0330

    DarKTeaM.lua

--- bot/beyondbot.lua
+++ bot/beyondbot.lua
@@ -294,27 +294,10 @@ function create_config( )
     sudo_users = {157059515},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
-    about_text = [[😎Tele Beyond Final Version😎
-➡️An Administrator The PowerFull Bot Based On TeleSeed And Bilingual Written On Lua⬅️
-
-Source On GitHub :
-
-Https://Github.com/solid021/TeleBeyond
-
-▶️Sudo Users :
-
-Developer&Founder : @SoLiD021
-
-Developer&Manager : @MrHalix
-
-Developer&Manager : @idivanmanheb
+    about_text = [[😎Tele DarK Final Version😎
 
 #⃣Team Channel :
-✌️ @BeyondTeam ✌️
-
-❤️Special Thx To :
-Beyond Team
-And All My Friends :D
+✌️ @DarKTeaMoNe ✌️
 ]],
     help_text_realm = [[
 Realm Commands:
